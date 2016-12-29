96ef804e0af0acc3c5bc653d426cede6baf66e07
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-29 17:08:56 -0600

    create table and re instantiate state with each searched city's data

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,4 +1,14 @@
-export default function(state=null, action){
+import {FETCH_WEATHER} from '../actions/index';
+
+export default function(state=[], action){
     // console.log('Action received', action);
+    switch (action.type){
+        case FETCH_WEATHER:
+
+        //this line below doesn't mutate the state but rather returns a new instance of state in order to save all cities' data a user has searched. 
+        return state.concat([action.payload.data]);
+        // same thing can also be written in ES6 syntax as:
+        // return [action.payload.data, ...state];
+    }
     return state;
 }

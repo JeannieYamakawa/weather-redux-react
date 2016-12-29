0660a93756f037dede71e93b38b85b6088dd0429
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-29 15:54:50 -0600

    binds onInputChange to searchBar instance

---
+++ src/containers/search-bar.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+
+export default class SearchBar extends Component{
+    constructor(props){
+        super(props);
+    //sets initial component-level state only
+        this.state = {term: ''};
+
+        //the purpose for the line below is to bind the context of onInputChange's 'this' to the return onChange callback function
+        //the right side of the equals sign's 'this' is the instance of the searchbar. it already has a function called 'onInputChange,' but it's saying 'bind that function to 'this', which is search bar, and then replace the existing function with it (right side of =)'...in other words, overwriting the local method.
+        //perform this line below whenever the callback has a reference to 'this'
+        this.onInputChange = this.onInputChange.bind(this);
+        //!!!^^^!!!^^^!!!^^^!!!^^^!!!
+    }
+
+
+    onInputChange(event){
+        // console.log(event.target.value);
+        this.setState({ term: event.target.value })
+    }
+
+
+    render(){
+        return(
+            <form className='input-group'>
+                <input
+                placeholder='Get a five-day forecast in your favorite cities'
+                className='form-control'
+                value={this.state.term}
+                onChange={this.onInputChange}
+                />
+                <span className='input-group-button'>
+                    <button type = 'submit'
+                        className='btn btn-secondary'>
+                        Submit
+                    </button>
+                </span>
+            </form>
+        );
+    }
+}
